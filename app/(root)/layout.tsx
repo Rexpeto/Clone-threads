31773c3ecaf9e1724cd9139ba46ba76247da4897
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-08-12 04:19:28 -0400

    feat: homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,8 @@
 import "@/app/globals.css";
+import Bottombar from "@/components/shared/Bottombar";
+import Leftbar from "@/components/shared/Leftbar";
+import Rightbar from "@/components/shared/Rightbar";
+import Topbar from "@/components/shared/Topbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -18,7 +22,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-dark-1`}>
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider>
+          <Topbar />
+          <main className="flex flex-row">
+            <Leftbar />
+            <section className="main-container">
+              <div className="max-w-4xl w-full">{children}</div>
+            </section>
+            <Rightbar />
+          </main>
+          <Bottombar />
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
-
 const Home = () => {
   return (
-    <main>
-      <UserButton afterSignOutUrl="/" />
-    </main>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 };
 
