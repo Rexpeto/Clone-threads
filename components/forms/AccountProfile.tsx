78b14672c37e7e8510c2142506e6f49541964847
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-08-20 01:21:10 -0400

    Onboarding page and uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.action";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -34,6 +36,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(userValidation),
@@ -81,7 +85,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    //TODO: Update user proile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
   };
 
   return (
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-export const connecToDB = async () => {
+export const connectToDB = async () => {
   mongoose.set("strict", true);
 
   if (!process.env.MONGODB_URI) return console.log("MONGODB_URI not found");
