b8d47831b13f8639fc1181d2a067112062514860
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-08-20 16:31:26 -0400

    Threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
-const Home = () => {
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.action";
+import { currentUser } from "@clerk/nextjs/server";
+
+const Home = async () => {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post.id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAd={post.createdAd}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 };
--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -37,3 +37,72 @@ export const createThread = async ({
     console.log(`Failed create thread error: ${error.message}`);
   }
 };
+
+export const fetchPosts = async (pageNumber = 1, pageSize = 20) => {
+  connectToDB();
+
+  //? Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+};
+
+export const fetchThreadById = async (id: string) => {
+  connectToDB();
+
+  try {
+    //TODO: populate Community
+
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          { path: "author", model: User, select: "_id id name parentId image" },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error) {
+    console.log(error);
+  }
+};
